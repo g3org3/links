f09739f04c22a6a68d35106c6034e5e5c11503a9
george <7jagjag@gmail.com>
2023-06-04 17:55:20 +0200

    :bug: fix lint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -109,7 +109,10 @@ const Home: NextPage = () => {
         <title>Links</title>
         <meta name="description" content="Links app" />
         <link rel="icon" type="image/svg+xml" href="/logo.svg" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
         <link rel="manifest" href="/manifest.json" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
