490f374a4e72dd2f00e097895780230dcbf0384f
george <7jagjag@gmail.com>
2023-06-21 16:53:12 +0200

    update with meta info

--- links-cli/cli.mjs
+++ links-cli/cli.mjs
@@ -8,6 +8,7 @@ import { ArgumentParser } from 'argparse'
 import { $ } from 'execa'
 import fs from 'fs/promises'
 import os from 'os'
+import { input } from '@inquirer/prompts'
 
 process.env.NODE_NO_WARNINGS = "1"
 
@@ -130,8 +131,7 @@ async function waitTilAuthenticated(client, host) {
 
 async function main() {
   const parser = new ArgumentParser({ description: 'Links cli' })
-  parser.add_argument('-n', { dest: 'newlink', help: 'add a new link' })
-  parser.add_argument('-s', { dest: 'search', help: 'search for links' })
+  parser.add_argument('--new', { dest: 'newlink', help: 'add a new link', action: 'store_true' })
   let args = parser.parse_args()
 
   if (!args.newlink && !args.search) {
@@ -148,15 +148,32 @@ async function main() {
   }
 
   if (args.newlink) {
-    console.log('create new link', args.newlink)
+    const newlink = await input({ message: 'Enter your link:' })
+    const details = await scrappeUrl(newlink)
+    for (const key of Object.keys(details)) {
+      console.log(`${chalk.blue(key)}:`, details[key])
+    }
+    
+    const tagstr = await input({ message: 'Enter tags [,]:' })
+    const tags = tagstr.split(',').filter(Boolean).concat([newlink.split('/')[2]]).concat(details.tags)
+    
+    console.log('create new link', newlink, 'with tags:', tags)
     const spinner = ora('creating new link').start()
-    await client.collection('links').create({ url: args.newlink, author: user.id })
+
+    // await client.collection('links').create({ url: newlink, author: user.id, tags })
     spinner.succeed("Created!")
   }
 
   if (args.search) {
     const spinner = ora(`searching links with "${args.search}"`).start()
-    const links = await client.collection('links').getList(1, 10, { filter: `url ~ "${args.search}" || desc ~ "${args.search}" ` })
+    /** @type{import('pocketbase').ListResult<import('pocketbase').Record> | null} */
+    let links = null
+    if (args.search.includes('tag:')) {
+      const tag = args.search.slice(4)
+      links = await client.collection('links').getList(1, 10, { filter: `tags:each ~ "${tag}"` })
+    } else {
+      links = await client.collection('links').getList(1, 10, { filter: `url ~ "${args.search}" || desc ~ "${args.search}" ` })
+    }
     spinner.succeed("Results:\n")
     for (const link of links.items) {
       console.log('---')
@@ -168,6 +185,75 @@ async function main() {
   }
 
   console.log("")
+}
+
+/**
+* @param {string} url
+*/
+async function scrappeUrl(url) {
+  const res = await fetch(url)
+  const text = await res.text()
+  const metaTags = getMetaTags(text)
+  const keywords = metaTags.filter((text) => text.includes('keywords'))
+  const descriptions = metaTags.filter((text) => text.includes('description'))
+  const images = metaTags.filter((text) => text.includes(':image"'))
+
+  // console.log({ images, descriptions })
+  const [desc] = getMetaTagContents(descriptions[0] || '')
+  const [image] = getMetaTagContents(images[0] || '')
+  const [tags] = getMetaTagContents(keywords[0] || '')
+  const [title] = getTitle(text)
+
+  return { desc, image, title, tags: (tags||'').split(',').map(x => x.trim()).filter(Boolean) }
+}
+
+
+/**
+* @param {string} html
+*/
+function getMetaTagContents(html) {
+  const regex = /<meta[^>]*content="([^"]*)"[^>]*>/gi
+  const matches = []
+  let match
+
+  // Find all matches
+  while ((match = regex.exec(html)) !== null) {
+    matches.push(match[1])
+  }
+
+  return matches
+}
+
+/**
+  * @param {string} html
+  */
+function getTitle(html) {
+  const regex = /<title>([^<]+)<\/title>/gi
+  const matches = []
+  let match
+
+  // Find all matches
+  while ((match = regex.exec(html)) !== null) {
+    matches.push(match[1])
+  }
+
+  return matches
+}
+
+/**
+  * @param {string} html
+  */
+function getMetaTags(html) {
+  const regex = /<meta[^>]*>/gi
+  const matches = []
+  let match
+
+  // Find all matches
+  while ((match = regex.exec(html)) !== null) {
+    matches.push(match[0])
+  }
+
+  return matches
 }
 
 
